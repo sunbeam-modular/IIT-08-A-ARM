b56b6962eac5a887aee78904ad78a699e3a67472
Precatsunbeam <sunbeamprecat@gmail.com>
2025-12-17 15:26:56 +0530

    day-4

---
+++ Day-4/Led.c/Inc/my_led.h
@@ -0,0 +1,21 @@
+/*
+ * my_led.h
+ *
+ *  Created on: Dec 17, 2025
+ *      Author: kiran_z6dopa8
+ */
+
+#ifndef MY_LED_H_
+#define MY_LED_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include"stm32f407xx.h"
+
+#define BV(n) (1 << (n))
+
+void led_init(void);
+void led_on(void);
+void led_off(void);
+
+#endif /* MY_LED_H_ */
